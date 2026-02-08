cab279e65fd690b917d81a2b0cdff64151718436
Ahmad Rafi'i <70046808+rafia9005@users.noreply.github.com>
2026-02-08 11:02:55 +0700

    Update label.tsx

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import * as LabelPrimitive from "@radix-ui/react-label"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
@@ -18,6 +19,7 @@ const Label = React.forwardRef<
     {...props}
   />
 ))
+
 Label.displayName = LabelPrimitive.Root.displayName
 
 export { Label }
